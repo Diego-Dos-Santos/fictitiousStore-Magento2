3a74c472df94c3562ef592181db9ddfc3ce2c2f8
Diego Dos Santos <diegodossantos@MacBook-Pro.local>
2020-10-21 01:37:04 +0200

    Translate and Frontend

--- app/etc/config.php
+++ app/etc/config.php
@@ -224,6 +224,8 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Mageplaza_Core' => 1,
+        'Mageplaza_BannerSlider' => 1,
         'ProgrammingAtKstark_CatewithimgWidget' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
